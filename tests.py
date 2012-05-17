2bd2c5109edf87f83ea9b517f91a2fa05ff1a88d
Randall Degges <rdegges@gmail.com>
2012-05-16 17:20:20 -0700

    Adding test case to ensure amazonify requires an affiliate tag.
    If we don't provide one, what point is there?

--- tests.py
+++ tests.py
@@ -7,3 +7,6 @@ class Amazonify(TestCase):
 
     def test_requires_url(self):
         self.assertRaises(TypeError, amazonify)
+
+    def test_requires_affiliate_tag(self):
+        self.assertRaises(TypeError, amazonify, 'http://www.amazon.com/PostgreSQL-High-Performance-Gregory-Smith/dp/184951030X/ref=trdrt_tipp_dp_img_GWTB_507846?pf_rd_p=1367759962&pf_rd_s=right-4&pf_rd_t=101&pf_rd_i=507846&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=1216X6HJC7KEWY0X3VD7')
