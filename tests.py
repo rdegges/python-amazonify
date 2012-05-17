9c1433f9799a6dacdf46df11a44a094b9b7390d8
Randall Degges <rdegges@gmail.com>
2012-05-16 18:22:42 -0700

    Adding stub test: test_returns_none_for_invalid_urls.
    amazonify should return None if the URL passed is invalid.

--- tests.py
+++ tests.py
@@ -15,6 +15,9 @@ class Amazonify(TestCase):
     def test_returns_string(self):
         self.assertIsInstance(amazonify('http://www.amazon.com/PostgreSQL-High-Performance-Gregory-Smith/dp/184951030X/ref=trdrt_tipp_dp_img_GWTB_507846?pf_rd_p=1367759962&pf_rd_s=right-4&pf_rd_t=101&pf_rd_i=507846&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=1216X6HJC7KEWY0X3VD7', 'rdegges-20'), str)
 
+    def test_returns_none_for_invalid_urls(self):
+        pass
+
     def test_strips_original_querystrings(self):
         parsed_url = urlparse('http://www.amazon.com/PostgreSQL-High-Performance-Gregory-Smith/dp/184951030X/ref=trdrt_tipp_dp_img_GWTB_507846?pf_rd_p=1367759962&pf_rd_s=right-4&pf_rd_t=101&pf_rd_i=507846&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=1216X6HJC7KEWY0X3VD7')
         self.assertNotIn(parsed_url.query, amazonify(parsed_url.geturl(), 'rdegges-20'))
