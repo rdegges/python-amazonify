81f18846f7b13f9cce7b66a5a672600f73256971
Randall Degges <rdegges@gmail.com>
2012-05-16 18:17:19 -0700

    Implementing code to pass test_strips_original_querystrings.
    The first thing we now do is strip all querystrings from the URL and return the
    URL.

--- amazonify.py
+++ amazonify.py
@@ -1,5 +1,13 @@
 """The simplest way to build Amazon Affiliate links, in Python."""
 
 
+from urlparse import urlparse, urlunparse
+
+
 def amazonify(url, affiliate_tag):
-    return url
+
+    # Firstly, remove all querystrings from the URL:
+    new_url = urlparse(url)
+    new_url = new_url[:4] + ('',) + new_url[5:]
+
+    return urlunparse(new_url)
