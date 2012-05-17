7c3a276d7e1f51cbd0cde9a91fb3c995f1e99668
Randall Degges <rdegges@gmail.com>
2012-05-16 18:37:30 -0700

    Making our querystring test a bit more robust.

--- tests.py
+++ tests.py
@@ -26,5 +26,10 @@ class Amazonify(TestCase):
         self.assertNotIn(parsed_url.query, amazonify(parsed_url.geturl(), 'rdegges-20'))
 
     def test_adds_affiliate_tag_as_a_querystring(self):
+        # Test it on a URL that already has a querystring:
         test_url = amazonify('http://www.amazon.com/PostgreSQL-High-Performance-Gregory-Smith/dp/184951030X/ref=trdrt_tipp_dp_img_GWTB_507846?pf_rd_p=1367759962&pf_rd_s=right-4&pf_rd_t=101&pf_rd_i=507846&pf_rd_m=ATVPDKIKX0DER&pf_rd_r=1216X6HJC7KEWY0X3VD7', 'rdegges-20')
+        self.assertEqual(urlparse(test_url).query, 'tag=rdegges-20')
+
+        # Test it on a URL that has no querystring:
+        test_url = amazonify('http://www.amazon.com/PostgreSQL-High-Performance-Gregory-Smith/dp/184951030X/ref=trdrt_tipp_dp_img_GWTB_507846', 'rdegges-20')
         self.assertEqual(urlparse(test_url).query, 'tag=rdegges-20')
